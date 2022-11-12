884415731befea924c3bb462366fa01cdf15ecf6
Kraymus <Kraymus@users.noreply.github.com>
2022-11-12 15:38:59 +0100

    Deleted scripts that was used to learn and test

--- Assets/Scripts/CustomList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CustomList : MonoBehaviour
-{
-    [SerializeField]
-    private List<AudioGroup> audioGroups = new List<AudioGroup>();
-}
--- Assets/Scripts/Tester.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tester : MonoBehaviour
-{
-    [SerializeField]
-    public List<AudioGroup> audioGroups;
-}
